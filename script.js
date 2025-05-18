bf5b4f1f88f4b0f48c97a87a0e47096412d350c3
Kaung Khant Ko <106913873+KaungBeingKhant@users.noreply.github.com>
2025-05-18 17:28:51 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const uploadInput = document.getElementById('upload-photo');
+const preview = document.getElementById('preview');
+
+uploadInput.addEventListener('change', function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+      preview.style.display = 'block';
+    };
+    reader.readAsDataURL(file);
+  }
+});
